c898ae39f467e0493db22b0a9e34ed63c6232725
Eric Osburn <bjonesen@gmail.com>
2013-11-08 19:43:26 -0600

    Update parser.h
     - Add in most operator functions
     - Finish most parsing functions
     - Set up skeleton for operators
     - Change to using deques for the stacks

--- includes/parser.h
+++ includes/parser.h
@@ -3,17 +3,20 @@
 
 #include <vector>
 #include <string>
+#include <map>
+#include <sstream>
 #include <iostream>
 #include <fstream>
 #include <stack>
 #include <exception>
 #include <algorithm>
+#include <cmath>
 
 #define DEBUG
 
 class SyntaxError : public std::exception {
 public:
-    SyntaxError( const char* strptr ) { this->str = std::string( strptr ); };
+    SyntaxError( const char* strptr) { this->str = std::string( strptr ); };
     virtual const char* what( ) const throw( ){
         return this->str.c_str( );
     };
@@ -30,46 +33,40 @@ public:
 
     class Value {
     public:
-        Value( ) { this->isVar = false; this->value = 0.0; };
+        Value( ) { this->isVar = false; this->value = 0.0; this->tag = 0; };
+        static Value Variable( char tag ) { Value temp; temp.isVar = true; temp.value = 0.0; temp.tag = tag; return temp; };
+        static Value Literal( float value ) { Value temp; temp.isVar = false; temp.value = value; temp.tag = 0; return temp; };
+        ~Value( ) { };
+
+        float getVal( std::map<char, float>& vList ) { if( this->isVar ) return vList[ this->tag ]; else return this->value; };
+        float setVal( std::map<char, float>& vList, float value ) { if( this->isVar ) vList[ this->tag ] = value; else throw( SyntaxError( "Assignment target not a variable!" ) ); };
 
         bool isVar;
         float value;
-        std::string tag;
-    };
-    class Variable : Value {
-    public:
-        Variable( float value, std::string& tag ) { this->isVar = true; this->value = value; this->tag = tag; };
-        ~Variable( ) { };
-    };
-    class Literal : Value {
-    public:
-        Literal( float value ) { this->value = value; };
-        ~Literal( ) { };
+        char tag;
     };
 
     class Control {
     public:
         Control( ) { this->type = Parser::CONTROL_INVALID; };
+        static Control cIf( bool branch ) { Control temp; temp.type = Parser::CONTROL_IF; temp.branch = branch; return temp; };
+        static Control cGosub( unsigned line ) { Control temp; temp.type = Parser::CONTROL_GOSUB; temp.line = line; return temp; };
+        static Control cWhile( unsigned line, Value& var ) { if( var.isVar == true ){ Control temp; temp.line = line; temp.var = var.tag; return temp; } else { throw SyntaxError( "While not initialized with a variable! " ); } };
         ~Control( ) { };
 
         Parser::ControlType type;
         unsigned line;
-    };
-    class cIf : Control {
-        cIf( bool branch ) { this->type = Parser::CONTROL_IF; this->branch = branch; };
-        ~cIf( ) { };
-
+        char var;
         bool branch;
     };
-    class cGoSub : Control {
-        cGoSub( unsigned line ) { this->type = Parser::CONTROL_GOSUB; this->line = line; };
-        ~cGoSub( ) { };
-    };
-    class cWhile : Control {
-        cWhile( unsigned line, Value& var ) { if( var.isVar == true ){ this->line = line; this->var = var.tag; } else { throw SyntaxError( "While not initialized with a variable" ); } };
-        ~cWhile( ) { };
 
-        std::string var;
+    class Operator{
+    public:
+        Operator( unsigned numArgs, void (* func)(std::stack<Parser::Value>&) ) { this->numArgs = numArgs; this->doFunc = func; };
+        ~Operator( ) { };
+
+        unsigned numArgs;
+        void (* doFunc)(std::stack<Parser::Value>&);
     };
 
     std::vector<unsigned short> linestarts;
@@ -78,9 +75,12 @@ public:
 
     bool running;
 
-    std::stack<Parser::Value> vStack;
+    std::deque<Parser::Value> vStack;
     std::deque<Parser::Control> cStack;
-    // TODO: std::map for de variables?
+    // use the back as a stack.
+    // i.e. use push_back, and pop_back. Also, .back() is the top of the stack
+    std::map<char, float> vList;
+    std::map<std::string, Operator> oList;
 
     int runParse( );
 
@@ -89,8 +89,187 @@ private:
 #endif
     void reset( );
     int openFile( std::string& );
+    void listProg( );
     int runFile( );
     int parseLine( unsigned );
+
+    // Ready for the loooong list of operators supported?? :D
+    // Basic format is this: void operatorname_( std::stack<Parser::Value>& stack ) {...};
+
+    // TODO:
+    /*
+    "RAND": operator(0, lambda: stack.append(literal(uniform(0,1)))),
+    "DUPE": operator(1, lambda: stack.append(stack[-1])),
+    "POP":  operator(1, lambda: print("Line:",cline+1,stack.pop())),
+    "PEEK": operator(1, lambda: print("Line:",cline+1,stack[-1])),
+    "PRNT": operator(0, lambda: print(stack,"\n",cstack)),
+    "LINE": operator(0, lambda: print("Line:",cline+1)),
+    "WAIT": operator(1, lambda: sleep(stack.pop().value)),
+
+    def input_():
+    */
+    
+    // The on basic one!
+    void assign_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // Get rhs and lhs values
+        stack[len-2].setVal( this->vList, stack[len-1].getVal( this->vList ) );
+        stack.pop_back( ); stack.pop_back( );
+    };
+
+    // Arithmetic time!
+    void add_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( stack[len-2].getVal( this->vList ) + stack[len-1].getVal( this->vList ) );
+        stack.pop_back( );
+    };
+    void sub_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( stack[len-2].getVal( this->vList ) - stack[len-1].getVal( this->vList ) );
+        stack.pop_back( );
+    };
+    void mul_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( stack[len-2].getVal( this->vList ) * stack[len-1].getVal( this->vList ) );
+        stack.pop_back( );
+    };
+    void div_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( stack[len-2].getVal( this->vList ) / stack[len-1].getVal( this->vList ) );
+        stack.pop_back( );
+    };
+
+    // More advanced maths
+    void pow_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( pow( stack[len-2].getVal( this->vList ), stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+    void sqrt_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-1] = Value::Literal( sqrt( stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+    void mod_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( fmod( stack[len-2].getVal( this->vList ), stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+    void abs_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-1] = Value::Literal( abs( stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+
+    // Next up, trig!
+    void sin_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-1] = Value::Literal( sin( stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+    void cos_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-1] = Value::Literal( cos( stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+    void tan_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-1] = Value::Literal( tan( stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+    void asin_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-1] = Value::Literal( asin( stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+    void acos_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-1] = Value::Literal( acos( stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+    void atan_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-1] = Value::Literal( atan( stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+    void atnt_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( atan2( stack[len-2].getVal( this->vList ), stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+
+    // Now come the log functions!
+    void exp_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-1] = Value::Literal( exp( stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+    void log_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-1] = Value::Literal( log( stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+    void logt_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-1] = Value::Literal( log10( stack[len-1].getVal( this->vList ) ) );
+        stack.pop_back( );
+    };
+
+    // And last but not least, the comparators!
+    void grt_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( ( stack[len-2].getVal( this->vList ) > stack[len-1].getVal( this->vList ) ) ? 1.0f : 0.0f );
+        stack.pop_back( );
+    };
+    void lst_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( ( stack[len-2].getVal( this->vList ) < stack[len-1].getVal( this->vList ) ) ? 1.0f : 0.0f );
+        stack.pop_back( );
+    };
+    void geq_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( ( stack[len-2].getVal( this->vList ) >= stack[len-1].getVal( this->vList ) ) ? 1.0f : 0.0f );
+        stack.pop_back( );
+    };
+    void leq_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( ( stack[len-2].getVal( this->vList ) <= stack[len-1].getVal( this->vList ) ) ? 1.0f : 0.0f );
+        stack.pop_back( );
+    };
+    void eql_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( ( stack[len-2].getVal( this->vList ) == stack[len-1].getVal( this->vList ) ) ? 1.0f : 0.0f );
+        stack.pop_back( );
+    };
+    void neq_( std::deque<Parser::Value>& stack ){
+        unsigned len = stack.size( );
+        // In-place replace the result
+        stack[len-2] = Value::Literal( ( stack[len-2].getVal( this->vList ) != stack[len-1].getVal( this->vList ) ) ? 1.0f : 0.0f );
+        stack.pop_back( );
+    };
 };
 
 #endif
