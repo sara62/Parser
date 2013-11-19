78aa431d68fe1b2c8f9393a0610c9bd5a3fc7a71
Eric Osburn <bjonesen@gmail.com>
2013-11-19 17:52:55 -0600

    Update main.cpp
    Formatting fixes

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,10 +1,25 @@
+/*************************************************************************************
+* main.cpp
+* Shows/Tests usage of the PathFinder class.
+*
+* NOTE: This file can change at any time. It is for testing only.
+*************************************************************************************/
+
+/*************************************************************************************
+* Includes
+*************************************************************************************/
 #include "parser.h"
 
+/*************************************************************************************
+* Main -> entry point -> for testing various functionalities of the library
+*************************************************************************************/
 int main( ){
+    // Make a parser
     Parser parse;
 
     // Run the parser!
     parse.runParse( );
 
+    // YOU SPIN ME RIGHT ROUND BABY RIGHT ROUND
     while (1);
 }
